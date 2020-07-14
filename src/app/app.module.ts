03266ba131258520d16214bdf6ffd61764bfd114
gallotamas <tamas.gallo@falcon.io>
2020-07-14 10:59:57 +0200

    chore: bootstrap, register required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgReduxModule, NgRedux } from '@angular-redux/store';
-import { createLogger } from 'redux-logger';
+import { StoreModule, Store, REDUCER_FACTORY } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { createEpicMiddleware } from './angular-redux/redux-observable/createEpicMiddleware';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IAppState } from './store/IAppState';
-import { rootReducer } from './store/rootReducer';
+import { AppState, initialState } from './common/app-state';
+import { createRootReducer } from './common/root-reducer';
+import { SyncWithReduxEffects } from './ngRx/sync-with-redux-effect';
+import { rootEpic } from './angular-redux/root-epic';
+import { metaReducers, combineReducersFactory } from './ngRx';
+
+const rootReducer = createRootReducer() as any;
+
+export const NG_RX_STORE_PROVIDER = [
+  StoreModule.forRoot(rootReducer, { initialState, metaReducers }),
+  EffectsModule.forRoot([SyncWithReduxEffects]),
+];
+
+export const NG_RX_REDUCER_FACTORY = [
+  {
+      provide: REDUCER_FACTORY,
+      useFactory: () => combineReducersFactory
+  }
+];
 
 @NgModule({
   declarations: [
@@ -16,12 +35,18 @@ import { rootReducer } from './store/rootReducer';
     BrowserModule,
     AppRoutingModule,
     NgReduxModule,
+    ...NG_RX_STORE_PROVIDER,
   ],
-  providers: [],
+  providers: [...NG_RX_REDUCER_FACTORY],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<IAppState>) {
-    ngRedux.configureStore(rootReducer, { counter: 0 }, [createLogger()]);
+  constructor(ngRedux: NgRedux<AppState>, store: Store<AppState>) {
+    const epicMiddleware = createEpicMiddleware({
+      dependencies: { getNgRxStore: () => store }
+    });
+
+    ngRedux.configureStore(rootReducer, {} as any, [epicMiddleware]);
+    epicMiddleware.run(rootEpic);
   }
 }
